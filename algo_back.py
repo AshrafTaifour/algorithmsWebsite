ac0270fd968d1789bf2b122b5327cf5537a70633
Joachim Lerman <joachimlerman@gmail.com>
2020-05-09 11:44:48 -0700

    Added hello page and modified homepage

--- algo_back.py
+++ algo_back.py
@@ -3,5 +3,9 @@ app = Flask(__name__)
 
 
 @app.route('/') #this is for our home page as it is the root aka '/'
+def home_page():
+    return 'HomePage'
+
+@app.route('/hello') #this is for our home page as it is the root aka '/'
 def hello_world():
     return 'Hello, World!'
