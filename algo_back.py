5e3412c2d623c9b63916d14980f7816b5ec438a6
Ashraf Taifour <ashraftayfour@gmail.com>
2020-05-09 15:33:27 -0400

    polished homepage
    altered title and h1 for html file, added textbox POST function for a
    search.

--- algo_back.py
+++ algo_back.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask,request, render_template
 app = Flask(__name__)
 
 
 @app.route('/') #this is for our home page as it is the root aka '/'
 def home_page():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 @app.route('/hello')
 @app.route('/hello/<name>')
